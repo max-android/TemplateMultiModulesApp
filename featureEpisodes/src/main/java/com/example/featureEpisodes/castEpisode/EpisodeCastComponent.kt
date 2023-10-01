cdc9e00e165456a778e7c8dafaa92c42f9fdd82e
MaxAndroid <mobiledeveloperxxxl@gmail.com>
2023-10-01 10:55:34 +0300

    добавил episodes cast - ui

--- featureEpisodes/src/main/java/com/example/featureEpisodes/castEpisode/EpisodeCastComponent.kt
+++ featureEpisodes/src/main/java/com/example/featureEpisodes/castEpisode/EpisodeCastComponent.kt
@@ -1,14 +1,38 @@
 package com.example.featureEpisodes.castEpisode
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.example.common.BaseViewModel
 import com.example.coreUi.workComponents.DetailShimmerComponent
 import com.example.coreUi.workComponents.LoadError
+import com.example.domain.model.episodes.GuestCastEpisodesModel
 
 @Composable
 fun EpisodeCastComponent(navController: NavController, navBackStackEntry: NavBackStackEntry) {
@@ -24,7 +48,7 @@ private fun ObserveState(
     state?.let { episodeState ->
         when (episodeState) {
             is EpisodeCastSuccess -> {
-
+                EpisodeCastUi(episodeState.listCast)
             }
 
             is EpisodeCastLoading -> {
@@ -40,3 +64,82 @@ private fun ObserveState(
         }
     }
 }
+
+@Composable
+fun EpisodeCastUi(listCast: List<GuestCastEpisodesModel>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        itemsIndexed(listCast) { _, item ->
+            EpisodeCastItem(item)
+        }
+    }
+}
+
+@Composable
+private fun EpisodeCastItem(castItem: GuestCastEpisodesModel) {
+    Column(
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+            .shadow(8.dp, shape = RoundedCornerShape(24.dp))
+            .background(Color.White)
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp)),
+            model = castItem.peopleModel.originalImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Column(modifier = Modifier.weight(1f)) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    model = castItem.characterModel.mediumImage,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = castItem.characterModel.name
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    model = castItem.peopleModel.mediumImage,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                )
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = castItem.peopleModel.name
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .clip(RoundedCornerShape(24.dp)),
+            model = castItem.characterModel.originalImage,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+    }
+}
+
+
+
+

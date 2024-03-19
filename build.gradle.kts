3474fb698ad2322c9473dd21e44881a32611e152
MaxAndroid <mobiledeveloperxxxl@gmail.com>
2024-03-19 11:48:10 +0300

    обновление gradle - 2

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id ("com.android.application") version "8.3.0" apply false
+    id ("com.android.library") version "8.3.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id ("org.jetbrains.kotlin.jvm") version "1.9.22" apply false
+    id ("com.google.devtools.ksp") version "1.9.22-1.0.18" apply false
+    id ("com.google.dagger.hilt.android") version "2.51" apply false
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 //Определите блок dependencyResolutionManagement, в котором вы объявляете хранилища для зависимостей.
 // Gradle использует их для разрешения зависимостей библиотек, которые вы включаете в проект.
 // Вы также определяете RepositoriesMode. По умолчанию любые репозитории, которые вы объявляете в
-// build.gradle проекта, будут переопределять те, которые вы объявляете в settings.gradle.
+// build.gradle.kts проекта, будут переопределять те, которые вы объявляете в settings.gradle.
 // Установив repositoriesMode в FAIL_ON_PROJECT_REPOS, вы фактически принудительно завершаете
 // сборку с ошибкой, если какие-либо репозитории объявлены вне этого файла.
 dependencyResolutionManagement {
@@ -20,19 +20,19 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "TemplateMultiModulesApp"
-include ':app'
-include ':data'
-include ':domain'
-include ':featureRoot'
-include ':coreUi'
-include ':featureProfile'
-include ':navigation'
-include ':common'
-include ':featureShows'
-include ':featureEpisodes'
-include ':featureSeasons'
-include ':featurePeoples'
-include ':featureOpen'
-include ':featureArticles'
-include ':featureNews'
-include ':featurePlayer'
+include(":app")
+include(":data")
+include(":domain")
+include(":featureRoot")
+include(":coreUi")
+include(":featureProfile")
+include(":navigation")
+include(":common")
+include(":featureShows")
+include(":featureEpisodes")
+include(":featureSeasons")
+include(":featurePeoples")
+include(":featureOpen")
+include(":featureArticles")
+include(":featureNews")
+include(":featurePlayer")
